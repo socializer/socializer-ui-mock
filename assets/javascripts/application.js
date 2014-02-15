ed80defae06221bdfbb8f007b5a4aa67be8330b0
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2014-02-15 13:19:12 -0500

    Site updated to 3ef0adb

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -11963,87 +11963,78 @@ the specific language governing permissions and limitations under the Apache Lic
 
 }(jQuery));
 (function() {
-  var emojis, emojis_list, hash_tags, names;
-
-  $("[title]").qtip({
-    style: {
-      classes: "qtip-bootstrap",
-      tip: {
-        width: 12
+  jQuery(function() {
+    var emojis, emojis_list, hash_tags, names;
+    $("[title]").qtip({
+      style: {
+        classes: "qtip-bootstrap",
+        tip: {
+          width: 12
+        }
+      },
+      show: {
+        solo: true
+      },
+      position: {
+        my: "top center",
+        at: "bottom center",
+        viewport: $(window)
       }
-    },
-    show: {
-      solo: true
-    },
-    position: {
-      my: "top center",
-      at: "bottom center",
-      viewport: $(window)
-    }
-  });
-
-  emojis = ["smile", "iphone", "girl", "smiley", "heart", "kiss", "copyright", "coffee", "a", "ab", "airplane", "alien", "ambulance", "angel", "anger", "angry", "arrow_forward", "arrow_left", "arrow_lower_left", "arrow_lower_right", "arrow_right", "arrow_up", "arrow_upper_left", "arrow_upper_right", "art", "astonished", "atm", "b", "baby", "baby_chick", "baby_symbol", "balloon", "bamboo", "bank", "barber", "baseball", "basketball", "bath", "bear", "beer", "beers", "beginner", "bell", "bento", "bike", "bikini", "bird", "birthday", "black_square", "blue_car", "blue_heart", "blush", "boar", "boat", "bomb", "book", "boot", "bouquet", "bow", "bowtie", "boy", "bread", "briefcase", "broken_heart", "bug", "bulb", "person_with_blond_hair", "phone", "pig", "pill", "pisces", "plus1", "point_down", "point_left", "point_right", "point_up", "point_up_2", "police_car", "poop", "post_office", "postbox", "pray", "princess", "punch", "purple_heart", "question", "rabbit", "racehorse", "radio", "up", "us", "v", "vhs", "vibration_mode", "virgo", "vs", "walking", "warning", "watermelon", "wave", "wc", "wedding", "whale", "wheelchair", "white_square", "wind_chime", "wink", "wink2", "wolf", "woman", "womans_hat", "womens", "x", "yellow_heart", "zap", "zzz", "+1", "-1"];
-
-  emojis_list = $.map(emojis, function(value, i) {
-    return {
-      id: i,
-      name: value
-    };
-  });
-
-  names = ["Jacob", "Isabella", "Ethan", "Emma", "Michael", "Olivia", "Alexander", "Sophia", "William", "Ava", "Joshua", "Emily", "Daniel", "Madison", "Jayden", "Abigail", "Noah", "Chloe", "Dominic", "Martin", "Tom"];
-
-  names = $.map(names, function(value, i) {
-    return {
-      id: i,
-      name: value,
-      email: value + "@email.com"
-    };
-  });
-
-  hash_tags = ["socializer", "social", "tag", "blog", "code", "caturday"];
-
-  hash_tags = $.map(hash_tags, function(value, i) {
-    return {
-      id: i,
-      name: value
-    };
-  });
-
-  $("#note-content-input").atwho({
-    at: "@",
-    data: names,
-    tpl: "<li data-value='@${name}'>${name} <small>${email}</small></li>",
-    show_the_at: true
-  }).atwho({
-    at: "#",
-    tpl: "<li data-value=\"#${name}\">${name}</li>",
-    data: hash_tags
-  }).atwho({
-    at: ":",
-    tpl: "<li data-value=':${name}:'><img src='http://a248.e.akamai.net/assets.github.com/images/icons/emoji/${name}.png' height='20' width='20'/> ${name} </li>",
-    data: emojis_list
-  });
-
-  $("#event-time-input .date").pikaday();
-
-  $("#event-time-input .time").timepicker({
-    scrollDefaultNow: true,
-    showDuration: true,
-    timeFormat: "g:ia"
-  });
-
-  $("#event-time-input").datepair({
-    parseDate: function($input) {
-      var picker;
-      picker = $input.data("pikaday");
-      return picker.getDate();
-    },
-    updateDate: function($input, dateObj) {
-      var picker;
-      picker = $input.data("pikaday");
-      return picker.setDate(dateObj);
-    }
+    });
+    emojis = ["smile", "iphone", "girl", "smiley", "heart", "kiss", "copyright", "coffee", "a", "ab", "airplane", "alien", "ambulance", "angel", "anger", "angry", "arrow_forward", "arrow_left", "arrow_lower_left", "arrow_lower_right", "arrow_right", "arrow_up", "arrow_upper_left", "arrow_upper_right", "art", "astonished", "atm", "b", "baby", "baby_chick", "baby_symbol", "balloon", "bamboo", "bank", "barber", "baseball", "basketball", "bath", "bear", "beer", "beers", "beginner", "bell", "bento", "bike", "bikini", "bird", "birthday", "black_square", "blue_car", "blue_heart", "blush", "boar", "boat", "bomb", "book", "boot", "bouquet", "bow", "bowtie", "boy", "bread", "briefcase", "broken_heart", "bug", "bulb", "person_with_blond_hair", "phone", "pig", "pill", "pisces", "plus1", "point_down", "point_left", "point_right", "point_up", "point_up_2", "police_car", "poop", "post_office", "postbox", "pray", "princess", "punch", "purple_heart", "question", "rabbit", "racehorse", "radio", "up", "us", "v", "vhs", "vibration_mode", "virgo", "vs", "walking", "warning", "watermelon", "wave", "wc", "wedding", "whale", "wheelchair", "white_square", "wind_chime", "wink", "wink2", "wolf", "woman", "womans_hat", "womens", "x", "yellow_heart", "zap", "zzz", "+1", "-1"];
+    emojis_list = $.map(emojis, function(value, i) {
+      return {
+        id: i,
+        name: value
+      };
+    });
+    names = ["Jacob", "Isabella", "Ethan", "Emma", "Michael", "Olivia", "Alexander", "Sophia", "William", "Ava", "Joshua", "Emily", "Daniel", "Madison", "Jayden", "Abigail", "Noah", "Chloe", "Dominic", "Martin", "Tom"];
+    names = $.map(names, function(value, i) {
+      return {
+        id: i,
+        name: value,
+        email: value + "@email.com"
+      };
+    });
+    hash_tags = ["socializer", "social", "tag", "blog", "code", "caturday"];
+    hash_tags = $.map(hash_tags, function(value, i) {
+      return {
+        id: i,
+        name: value
+      };
+    });
+    $("#note-content-input").atwho({
+      at: "@",
+      data: names,
+      tpl: "<li data-value='@${name}'>${name} <small>${email}</small></li>",
+      show_the_at: true
+    }).atwho({
+      at: "#",
+      tpl: "<li data-value=\"#${name}\">${name}</li>",
+      data: hash_tags
+    }).atwho({
+      at: ":",
+      tpl: "<li data-value=':${name}:'><img src='http://a248.e.akamai.net/assets.github.com/images/icons/emoji/${name}.png' height='20' width='20'/> ${name} </li>",
+      data: emojis_list
+    });
+    $("#event-time-input .date").pikaday();
+    $("#event-time-input .time").timepicker({
+      scrollDefaultNow: true,
+      showDuration: true,
+      timeFormat: "g:ia"
+    });
+    return $("#event-time-input").datepair({
+      parseDate: function($input) {
+        var picker;
+        picker = $input.data("pikaday");
+        return picker.getDate();
+      },
+      updateDate: function($input, dateObj) {
+        var picker;
+        picker = $input.data("pikaday");
+        return picker.setDate(dateObj);
+      }
+    });
   });
 
 }).call(this);
