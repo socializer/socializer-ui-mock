863aaed1a8626fff5cf4f35a1ccc732568e767c7
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2015-05-27 10:55:24 -0400

    remove optional settings

--- config.rb
+++ config.rb
@@ -46,10 +46,6 @@ commit_message = "Site updated to #{head}"
 # github deploy
 activate :deploy do |deploy|
   deploy.method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   deploy.commit_message = commit_message      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
 
