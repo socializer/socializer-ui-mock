a38d4bae27aa3e51c428efdfb39bee8a292f84c4
Tom Pietschker <tom.pietschker@acmetechnologygroup.com>
2018-02-16 08:40:14 -0500

    specify the port for livereload

--- config.rb
+++ config.rb
@@ -22,7 +22,7 @@ page '/*.txt', layout: false
 
 # Reload the browser automatically whenever files change
 configure :development do
-  activate :livereload
+  activate :livereload, port: 35730
 end
 
 # Methods defined in the helpers block are available in templates
